ac66359e2dc30efc847bf5dc0b65514584e396de
Haruki Hirai <iamprivateengineer@gmail.com>
2021-10-06 16:55:11 +0900

    [mod] adjust html css and js for mobile device

--- src/pages/Menu.vue
+++ src/pages/Menu.vue
@@ -72,11 +72,72 @@
     <div class="hidden-sm-and-up">
       <p>
         <img
-          src="../assets/pc-menu-bg.jpg"
+          src="../assets/mobile-menu-bg.jpg"
           alt="Menuの背景画像"
           class="small-device"
         />
       </p>
+      <v-list id="v-list" color="rgb(0, 0, 0, 0.0)">
+        <v-list-item-group v-model="selectedItem" color="#b60000">
+          <v-list-item
+            v-for="(burger, i) in burgers"
+            :key="`first-${i}`"
+          >
+            <v-list-item-content>
+              <v-list-item-title
+                v-text="burger.text"
+                :class="
+                  `font-weight-bold text-capture ${burger.name}`
+                "
+                @click="showBurgerImg(burger.name)"
+                @mousedown="deleteShowUp()"
+              ></v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+          <v-list-item
+            v-for="(item, j) in otherItems"
+            :key="`second-${j}`"
+          >
+            <v-list-item-content>
+              <v-list-item-title
+                v-text="item.text"
+                class="font-weight-bold text-capture"
+                @click="
+                  showOtherImg(item.name, item.description, item.text)
+                "
+              ></v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list-item-group>
+      </v-list>
+      <p class="burger-name-mb">{{ currentBurger.toUpperCase() }}</p>
+      <div class="picture-area-mb">
+        <p class="burger-picture-mb">
+          <img
+            :src="require(`../assets/${currentBurger}.png`)"
+            :alt="`${currentBurger}の画像`"
+            class="burger-img"
+          />
+        </p>
+      </div>
+      <transition>
+        <div class="modal-window" v-show="show">
+          <div class="mask" v-on:click="show = !show">
+            <div class="other-picture-mb">
+              <img
+                :src="require(`../assets/${currentOther}.png`)"
+                :alt="`${currentOther}の画像`"
+              />
+              <div class="other-description-mb">
+                <h2 class="text-center mb-1">
+                  {{ currentTitle }}
+                </h2>
+                {{ currentDescription }}
+              </div>
+            </div>
+          </div>
+        </div>
+      </transition>
     </div>
   </div>
 </template>
@@ -130,17 +191,27 @@ export default {
   },
   methods: {
     showBurgerImg: function(itemName) {
-      const burgerImg = this.$el.querySelector('.burger-img');
+      const burgerImgs = this.$el.querySelectorAll('.burger-img');
       const burgerName = this.$el.querySelector('.burger-name');
-      burgerImg.classList.add('show-up');
+      const mbBurgerName = this.$el.querySelector('.burger-name-mb');
+      burgerImgs.forEach((img) => {
+        img.classList.add('show-up');
+      });
+
       burgerName.classList.add('show-up');
+      mbBurgerName.classList.add('show-up');
+
       this.currentBurger = itemName;
     },
     deleteShowUp: function() {
-      const burgerImg = this.$el.querySelector('.burger-img');
+      const burgerImgs = this.$el.querySelectorAll('.burger-img');
       const burgerName = this.$el.querySelector('.burger-name');
-      burgerImg.classList.remove('show-up');
+      const mbBurgerName = this.$el.querySelector('.burger-name-mb');
+      burgerImgs.forEach((img) => {
+        img.classList.remove('show-up');
+      });
       burgerName.classList.remove('show-up');
+      mbBurgerName.classList.remove('show-up');
     },
 
     showOtherImg: function(name, description, text) {
@@ -170,13 +241,24 @@ export default {
   position: absolute;
   top: 0;
   left: 33%;
+  margin: 0 auto;
+  text-align: center;
+  width: 44%;
   font-size: 5vw;
   font-weight: bold;
   color: #b60000;
 }
 
-.burger-picture img {
-  width: 65vw;
+.burger-name-mb {
+  position: absolute;
+  bottom: 45%;
+  left: 20%;
+  margin: 0 auto;
+  text-align: center;
+  width: 20%;
+  font-size: 8vw;
+  font-weight: bold;
+  color: #b60000;
 }
 
 .picture-area {
@@ -184,6 +266,21 @@ export default {
   top: 100px;
   left: 50%;
   transform: translateX(-50%);
+}
+
+.picture-area-mb {
+  position: absolute;
+  bottom: 30px;
+  left: 50%;
+  transform: translateX(-50%);
+}
+
+.burger-picture img {
+  width: 65vw;
+}
+
+.burger-picture-mb img {
+  width: 100vw;
 }
 
 .modal-window {
@@ -215,6 +312,28 @@ export default {
   height: auto;
 }
 
+.other-picture-mb {
+  position: fixed;
+  top: 100px;
+  left: 50%;
+  transform: translateX(-50%);
+  background-color: #fff;
+  width: 90vw;
+  height: 90vw;
+  border: 5px solid #b60000;
+  border-radius: 10px;
+  z-index: 2;
+}
+
+.other-picture-mb img {
+  position: fixed;
+  left: 50%;
+  transform: translateX(-50%);
+  margin-top: 20px;
+  width: 80vw;
+  height: auto;
+}
+
 .other-description {
   position: absolute;
   left: 50%;
@@ -222,6 +341,21 @@ export default {
   font-size: 1.2vw;
   transform: translateX(-50%);
   width: 37vw;
+  height: auto;
+  background-color: #c62828;
+  color: #fff;
+  padding: 10px;
+  border-radius: 0 0 10px 10px;
+  z-index: 2;
+}
+
+.other-description-mb {
+  position: absolute;
+  left: 50%;
+  top: 57vw;
+  font-size: 3vw;
+  transform: translateX(-50%);
+  width: 80vw;
   height: auto;
   background-color: #c62828;
   color: #fff;
@@ -250,7 +384,7 @@ export default {
 .small-device {
   margin-top: -13px;
   margin-left: -36px;
-  width: 120vw;
+  width: 106vw;
 }
 
 .show-up {
