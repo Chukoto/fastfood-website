fbd615fe2553e4ebfbd8ef33f44b319d0255939d
Haruki Hirai <iamprivateengineer@gmail.com>
2021-10-07 11:49:56 +0900

    [mod] create header animation "open" and "fold" in Header.vue

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,67 +1,45 @@
 <template>
-  <div class="">
-    <div class="hidden-sm-and-down">
-      <v-row class="my-2 text-center align-center">
-        <v-col>
-          <router-link :to="'/'">
-            <img :src="logoPath" alt="ページのロゴ" width="100" />
-          </router-link>
-        </v-col>
-        <v-col
-          v-for="(headerMenu, index) in headerMenuList"
-          :key="index"
-        >
-          <button>
-            <router-link
-              :to="headerMenu.link"
-              class="text-decoration-none"
-            >
-              {{ headerMenu.title }}
+  <div>
+    <div class="header">
+      <div class="hidden-xs-only">
+        <v-row class="my-2 text-center align-center">
+          <v-col>
+            <router-link :to="'/'">
+              <img :src="logoPath" alt="ページのロゴ" width="100" />
             </router-link>
-          </button>
-        </v-col>
-      </v-row>
+          </v-col>
+          <v-col
+            v-for="(headerMenu, index) in headerMenuList"
+            :key="index"
+          >
+            <button>
+              <router-link
+                :to="headerMenu.link"
+                class="text-decoration-none"
+              >
+                {{ headerMenu.title }}
+              </router-link>
+            </button>
+          </v-col>
+        </v-row>
+      </div>
       <v-divider></v-divider>
-    </div>
-    <div class="hidden-md-and-up hidden-xs-only">
-      <v-row class="my-2 text-center align-center">
-        <v-col>
-          <router-link :to="'/'">
-            <img :src="logoPath" alt="ページのロゴ" width="80" />
-          </router-link>
-        </v-col>
-        <v-col
-          v-for="(headerMenu, index) in headerMenuList"
-          :key="index"
-          class="text-caption"
-        >
-          <button>
-            <router-link
-              :to="headerMenu.link"
-              class="text-decoration-none"
-            >
-              {{ headerMenu.title }}
+      <div class="hidden-sm-and-up">
+        <v-row class="my-2 text-center align-center">
+          <v-col
+            ><v-app-bar-nav-icon
+              @click="toggleSideNav"
+            ></v-app-bar-nav-icon
+          ></v-col>
+          <v-col>
+            <router-link :to="'/'">
+              <img :src="logoPath" alt="ページのロゴ" width="65" />
             </router-link>
-          </button>
-        </v-col>
-      </v-row>
-      <v-divider></v-divider>
-    </div>
-    <div class="hidden-sm-and-up">
-      <v-row class="my-2 text-center align-center">
-        <v-col
-          ><v-app-bar-nav-icon
-            @click="toggleSideNav"
-          ></v-app-bar-nav-icon
-        ></v-col>
-        <v-col>
-          <router-link :to="'/'">
-            <img :src="logoPath" alt="ページのロゴ" width="65" />
-          </router-link>
-        </v-col>
-        <v-spacer></v-spacer>
-      </v-row>
-      <v-divider></v-divider>
+          </v-col>
+          <v-spacer></v-spacer>
+        </v-row>
+        <v-divider></v-divider>
+      </div>
     </div>
   </div>
 </template>
@@ -84,12 +62,63 @@ export default {
       smallScreen: 'hidden-sm-and-down',
       mediumScreen: 'hidden-md-and-up hidden-xs-only',
       largeScreen: 'hidden-sm-and-up',
+      currentScrollY: 0,
+      preScrollY: 0,
     };
+  },
+  created() {
+    window.addEventListener('scroll', this.onScroll);
   },
   methods: {
     ...mapActions(['toggleSideNav']),
+    onScroll() {
+      const header = document.querySelector('.header');
+      this.preScrollY = this.currentScrollY;
+      this.currentScrollY = window.scrollY;
+      console.log(window.scrollY);
+      if (this.currentScrollY <= 60) {
+        header.classList.remove('fold');
+      } else if (this.preScrollY < this.currentScrollY) {
+        header.classList.remove('fold');
+        header.classList.add('open');
+      } else {
+        header.classList.remove('open');
+        header.classList.add('fold');
+      }
+    },
   },
 };
 </script>
 
-<style></style>
+<style>
+@import '../assets/keyframes/animation.css';
+.header {
+  position: fixed;
+  width: 100%;
+  z-index: 999;
+  background-color: #fff;
+  border-bottom: 6px solid #b60000;
+}
+.header::before {
+  content: '';
+  position: absolute;
+  top: -10px;
+  bottom: 3px;
+  left: -10px;
+  right: -10px;
+  border-bottom: 3px solid #b60000;
+  pointer-events: none;
+}
+
+.fold {
+  animation-name: fold;
+  animation-duration: 0.5s;
+  animation-fill-mode: forwards;
+}
+
+.open {
+  animation-name: open;
+  animation-duration: 0.5s;
+  animation-fill-mode: forwards;
+}
+</style>
